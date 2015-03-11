7d2552fd84c8bedc2eeeaa9d2be4a47111067c3b
Fox <thienduongvangem.ntt@gmail.com>
2015-03-11 14:59:26 +0700

    ok

--- footer.php
+++ footer.php
@@ -12,10 +12,37 @@ global $smof_data;
 ?>
         </div><!-- #main -->
 		<footer id="site-footer" class="site-footer" role="contentinfo">
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
+		    <div class="container">
+		        <div class="row">
+        			<div id="footer-top" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        			     <div class="row">
+    			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+    			             footer-top-1
+    			             </div>
+    			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+    			             footer-top-2
+    			             </div>
+    			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+    			             footer-top-3
+    			             </div>
+    			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+    			             footer-top-4
+    			             </div>
+        			     </div>
+        			</div>
+        			<div id="footer-bottom" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        			     <div class="row">
+    			             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+    			                 footer-bottom-1
+    			             </div>
+    			             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+    			                 footer-bottom-2
+    			             </div>
+        			     </div>
+        			</div>
+    			</div>
+			</div>
+		</footer>
 	</div><!-- #page -->
 
 	<?php wp_footer(); ?>
