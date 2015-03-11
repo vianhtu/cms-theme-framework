e1db254f30b49e450b9fdc3cedfb1681ae8c62d8
Fox <thienduongvangem.ntt@gmail.com>
2015-03-11 10:57:39 +0700

    ok

--- footer.php
+++ footer.php
@@ -10,11 +10,14 @@
  */
 global $smof_data;
 ?>
-<footer>
-	<!-- #main .wrapper -->
-</footer>
-</div><!-- #page -->
-<!-- #colophon -->
-<?php wp_footer(); ?>
+        </div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
 </body>
 </html>
