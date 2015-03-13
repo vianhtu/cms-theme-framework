2d53dfee43580bdb5b1d342865020abead57109b
CMS <support@joomlaman.com>
2015-03-13 10:25:48 +0700

    ok

--- footer.php
+++ footer.php
@@ -11,20 +11,22 @@
 global $smof_data;
 ?>
         </div><!-- #main -->
-			<footer id="footer-top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                        footer-top-1
-                        </div>
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                        footer-top-2
-                        </div>
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                        footer-top-3
-                        </div>
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                        footer-top-4
+			<footer>
+                <div id="footer-top">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                            footer-top-1
+                            </div>
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                            footer-top-2
+                            </div>
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                            footer-top-3
+                            </div>
+                            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                            footer-top-4
+                            </div>
                         </div>
                     </div>
                 </div>
