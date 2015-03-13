e28ece13f9f39d59d20317c82915a3e1a653572f
CMS <support@joomlaman.com>
2015-03-13 14:30:54 +0700

    ok

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -10,18 +10,22 @@
 
 get_header(); ?>
 <div id="page-front-page">
-	<div id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-		<div id="content" role="main">
+    <div class="container">
+        <div class="row">
+            <div id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                <div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
-				<?php comments_template( '', true ); ?>
-			<?php endwhile; // end of the loop. ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php get_template_part( 'content', 'page' ); ?>
+                        <?php comments_template( '', true ); ?>
+                    <?php endwhile; // end of the loop. ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
-	<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-    <?php get_sidebar(); ?>
+                </div><!-- #content -->
+            </div><!-- #primary -->
+            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
--- page-templates/left-sidebar.php
+++ page-templates/left-sidebar.php
@@ -10,18 +10,22 @@
 
 get_header(); ?>
 <div id="page-left-sidebar">
-    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-    <?php get_sidebar(); ?>
-    </div>
-	<div id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-		<div id="content" role="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                <?php get_sidebar(); ?>
+            </div>
+            <div id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                <div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
-				<?php comments_template( '', true ); ?>
-			<?php endwhile; // end of the loop. ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php get_template_part( 'content', 'page' ); ?>
+                        <?php comments_template( '', true ); ?>
+                    <?php endwhile; // end of the loop. ?>
 
-		</div><!-- #content -->
+                </div><!-- #content -->
+            </div><!-- #primary -->
+        </div><!-- #primary -->
 	</div><!-- #primary -->
 </div>
 <?php get_footer(); ?>
