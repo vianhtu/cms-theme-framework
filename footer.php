525c7fbeaf8c9c71f2d0f61dd9858323f2ea2240
CMS <support@joomlaman.com>
2015-03-13 10:20:28 +0700

    ok

--- footer.php
+++ footer.php
@@ -11,38 +11,37 @@
 global $smof_data;
 ?>
         </div><!-- #main -->
-
-        <div id="footer-top" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-             <div class="container">
-                 <div class="row">
-                     <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                     footer-top-1
-                     </div>
-                     <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                     footer-top-2
-                     </div>
-                     <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                     footer-top-3
-                     </div>
-                     <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                     footer-top-4
-                     </div>
-                 </div>
-             </div>
-        </div>
-        <div id="footer-bottom" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-             <div class="container">
-                 <div class="row">
-                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                         footer-bottom-1
-                     </div>
-                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                         footer-bottom-2
-                     </div>
-                 </div>
-             </div>
-        </div>
-
+			<footer id="footer-top">
+			     <div class="container">
+    			     <div class="row">
+			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			             footer-top-1
+			             </div>
+			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			             footer-top-2
+			             </div>
+			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			             footer-top-3
+			             </div>
+			             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			             footer-top-4
+			             </div>
+    			     </div>
+			     </div>
+	        </div>
+			<div id="footer-bottom">
+			     <div class="container">
+    			     <div class="row">
+    		             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+    		                 footer-bottom-1
+    		             </div>
+    		             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+    		                 footer-bottom-2
+    		             </div>
+    			     </div>
+			     </div>
+			</div>
+		</footer><!-- #site-footer -->
 	</div><!-- #page -->
 
 	<?php wp_footer(); ?>
