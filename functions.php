d8102deb45fa93b7285f2c9e08c390c82fa7c719
Fox <thienduongvangem.ntt@gmail.com>
2015-03-14 09:20:50 +0700

    ok

--- functions.php
+++ functions.php
@@ -293,6 +293,56 @@ function cms_widgets_init() {
 add_action( 'widgets_init', 'cms_widgets_init' );
 
 /**
+ * Display navigation to next/previous set of posts when applicable.
+ *
+ * @since Twenty Thirteen 1.0
+ */
+function cms_paging_nav() {
+    // Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+
+	$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+	$pagenum_link = html_entity_decode( get_pagenum_link() );
+	$query_args   = array();
+	$url_parts    = explode( '?', $pagenum_link );
+
+	if ( isset( $url_parts[1] ) ) {
+		wp_parse_str( $url_parts[1], $query_args );
+	}
+
+	$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+	$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+
+	$format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+	$format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
+
+	// Set up paginated links.
+	$links = paginate_links( array(
+			'base'     => $pagenum_link,
+			'format'   => $format,
+			'total'    => $GLOBALS['wp_query']->max_num_pages,
+			'current'  => $paged,
+			'mid_size' => 1,
+			'add_args' => array_map( 'urlencode', $query_args ),
+			'prev_text' => __( '&larr; Previous', THEMENAME ),
+			'next_text' => __( 'Next &rarr;', THEMENAME ),
+	) );
+
+	if ( $links ) :
+
+	?>
+	<nav class="navigation paging-navigation" role="navigation">
+			<div class="pagination loop-pagination">
+				<?php echo ''.$links; ?>
+			</div><!-- .pagination -->
+	</nav><!-- .navigation -->
+	<?php
+	endif;
+}
+
+/**
 * Display navigation to next/previous post when applicable.
 *
 * @since 1.0.0
