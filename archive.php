53a2cb8f5d4bb1c8e323af54a55590580cfae637
Fox <thienduongvangem.ntt@gmail.com>
2015-03-14 09:35:05 +0700

    ok

--- archive.php
+++ archive.php
@@ -12,32 +12,19 @@
  *
  * @link http://codex.wordpress.org/Template_Hierarchy
  *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @package CMSSuperHeroes
+ * @subpackage CMS Theme
+ * @since 1.0.0
  */
 
 get_header(); ?>
-<div class="<?php cms_main_class(); ?>">
+<div class="container">
     <div class="row">
         <section id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
             <div id="content" role="main">
 
             <?php if ( have_posts() ) : ?>
-                <header class="archive-header">
-                    <h1 class="archive-title"><?php
-                        if ( is_day() ) :
-                            printf( __( 'Daily Archives: %s', 'twentytwelve' ), '<span>' . get_the_date() . '</span>' );
-                        elseif ( is_month() ) :
-                            printf( __( 'Monthly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentytwelve' ) ) . '</span>' );
-                        elseif ( is_year() ) :
-                            printf( __( 'Yearly Archives: %s', 'twentytwelve' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentytwelve' ) ) . '</span>' );
-                        else :
-                            _e( 'Archives', 'twentytwelve' );
-                        endif;
-                    ?></h1>
-                </header><!-- .archive-header -->
-
+            
                 <?php
                 /* Start the Loop */
                 while ( have_posts() ) : the_post();
