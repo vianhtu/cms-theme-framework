92bb4be913ca12ef57c96394a542ca6ba09cd210
Fox <thienduongvangem.ntt@gmail.com>
2015-03-14 09:25:31 +0700

    ok

--- search.php
+++ search.php
@@ -19,14 +19,12 @@ get_header(); ?>
                     <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentytwelve' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
                 </header>
 
-                <?php cms_paging_nav(); ?>
-
                 <?php /* Start the Loop */ ?>
                 <?php while ( have_posts() ) : the_post(); ?>
                     <?php get_template_part( 'content', get_post_format() ); ?>
                 <?php endwhile; ?>
 
-                <?php twentytwelve_content_nav( 'nav-below' ); ?>
+                <?php cms_paging_nav(); ?>
 
             <?php else : ?>
 
