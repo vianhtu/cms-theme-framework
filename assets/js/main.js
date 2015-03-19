6563170fff602d83442b68881307217870787125
Fox <thienduongvangem.ntt@gmail.com>
2015-03-19 10:37:33 +0700

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,10 +109,10 @@ jQuery(document).ready(function($) {
 			var sub_x = sub_menu.offset().left;
 			var sub_w = sub_menu.outerWidth(true);
 			
+			console.log("x: "+sub_x + "w: " + sub_w);
+			
 			if(sub_x <= sub_w && (sub_x + sub_w) > window_width){
-				
-				console.log("x: "+sub_x + "w: " + sub_w);
-				
+					
 				if(!sub_menu.parents('ul').hasClass('ping-right')){
 					sub_menu.addClass('ping-right');
 				} else {
