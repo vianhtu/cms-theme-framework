ee995b7bc8e47721e1adf17e2475b736bada691b
Cong Nguyen <congnguyen@Congs.local>
2015-03-19 14:32:06 +0700

    update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,6 +2,15 @@
     "use strict";
     $(document).ready(function(){
         var $menu = $('.nav-menu');
-
+        $menu.find('ul.sub-menu > li').each(function(){
+            var $submenu = $(this).find('>ul');
+            if($submenu.length == 1){
+                $(this).hover(function(){
+                    if($submenu.offset().left + $submenu.width() > $(window).width()){
+                        console.log('Dap vao tuong roi');
+                    }
+                });
+            }
+        });
     });
 })(jQuery);
