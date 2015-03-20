f559f9842fbe5b2a5caf4f1de4f55d35b20301d0
Fox <thienduongvangem.ntt@gmail.com>
2015-03-20 14:49:38 +0700

    ok

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -15,11 +15,25 @@ get_header(); ?>
             <div id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
                 <div id="content" role="main">
 
-                    <?php while ( have_posts() ) : the_post(); ?>
-                        <?php get_template_part( 'content', 'page' ); ?>
-                        <?php comments_template( '', true ); ?>
-                    <?php endwhile; // end of the loop. ?>
-
+                <?php global $wp_query, $paged; ?>
+                
+                <?php $wp_query->query('post_type=post&showposts='.get_option('posts_per_page').'&paged='.$paged); ?>
+                
+                <?php if ( have_posts() ) : ?>
+                    <?php while ( have_posts() ) : the_post();
+                        /* Include the post format-specific template for the content. If you want to
+                         * this in a child theme then include a file called called content-___.php
+                         * (where ___ is the post format) and that will be used instead.
+                         */
+                        get_template_part( 'single-templates/content/content', get_post_format() );
+                    endwhile; // end of the loop.?>
+                    
+                    <?php cms_paging_nav(); ?>
+                    
+                <?php else : ?>
+                    <?php get_template_part( 'single-templates/content', 'none' ); ?>
+                <?php endif; ?> 
+                
                 </div><!-- #content -->
             </div><!-- #primary -->
             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
