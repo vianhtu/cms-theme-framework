ae766bb4182bf1cd8112ca8a9f0f8a6de1273243
Fox <thienduongvangem.ntt@gmail.com>
2015-03-27 09:37:38 +0700

    ok

--- vc_templates/vc_row.php
+++ vc_templates/vc_row.php
@@ -15,7 +15,7 @@ $row_style = '';
 if($bg_style == 'image'){
     $el_class .= " cms_parallax";
     $row_data .= " data-speed = $bd_p_speed";
-    $row_style .= "background-position: 50% 0;background-repeat:no-repeat;background-size:cover;";
+    $row_style .= "background-position: 50% 0;background-repeat:no-repeat;background-attachment:fixed;background-size:cover;";
 }
 
 
