ea5c189c69416a015abfab12be522927dea84461
Fox <thienduongvangem.ntt@gmail.com>
2015-04-10 14:49:03 +0700

    ok

--- inc/options/meta.options.php
+++ inc/options/meta.options.php
@@ -42,91 +42,93 @@ class CMSMetaOptions
 	           <li class="tab"><a href="#tabs-header"><?php _e('Header', THEMENAME); ?></a></li>
 	           <li class="tab"><a href="#tabs-page-title"><?php _e('Page Title', THEMENAME); ?></a></li>
 	        </ul>
-            <div id="tabs-general">
-            <?php
-            cms_options(array(
-                'id' => 'full_width',
-                'label' => __('Full Width',THEMENAME),
-                'type' => 'switch',
-                'options' => array('on'=>'1','off'=>''),
-            ));
-            ?>
-            </div>
-            <div id="tabs-header">
-            <?php
-            /* header. */
-            cms_options(array(
-                'id' => 'header',
-                'label' => __('Header',THEMENAME),
-                'type' => 'switch',
-                'options' => array('on'=>'1','off'=>''),
-                'follow' => array('1'=>array('#page_header_enable'))
-            ));
-            ?>  <div id="page_header_enable"><?php
+	        <div class='panel-container'>
+                <div id="tabs-general">
+                <?php
                 cms_options(array(
-                    'id' => 'header_layout',
-                    'label' => __('Layout',THEMENAME),
-                    'type' => 'imegesselect',
-                    'options' => array(
-                        '' => get_template_directory_uri().'/inc/options/images/header/h-default.png',
-                        '1' => get_template_directory_uri().'/inc/options/images/header/h-default.png',
-                        '2' => get_template_directory_uri().'/inc/options/images/header/h-default.png',
-                    )
+                    'id' => 'full_width',
+                    'label' => __('Full Width',THEMENAME),
+                    'type' => 'switch',
+                    'options' => array('on'=>'1','off'=>''),
                 ));
-                $menus = array();
-                $menus[''] = 'Default';
-                $obj_menus = wp_get_nav_menus();
-                foreach ($obj_menus as $obj_menu){
-                    $menus[$obj_menu->term_id] = $obj_menu->name;
-                }
-                $navs = get_registered_nav_menus();
-                foreach ($navs as $key => $mav){
-                    cms_options(array(
-                    'id' => $key,
-                    'label' => $mav,
-                    'type' => 'select',
-                    'options' => $menus
-                    ));
-                }
                 ?>
                 </div>
-            </div>
-            <div id="tabs-page-title">
-            <?php
-            /* page title. */
-            cms_options(array(
-                'id' => 'page_title',
-                'label' => __('Page Title',THEMENAME),
-                'type' => 'switch',
-                'options' => array('on'=>'1','off'=>''),
-                'follow' => array('1'=>array('#page_title_enable'))
-            ));
-            ?>  <div id="page_title_enable"><?php
+                <div id="tabs-header">
+                <?php
+                /* header. */
                 cms_options(array(
-                    'id' => 'page_title_text',
-                    'label' => __('Title',THEMENAME),
-                    'type' => 'text',
+                    'id' => 'header',
+                    'label' => __('Header',THEMENAME),
+                    'type' => 'switch',
+                    'options' => array('on'=>'1','off'=>''),
+                    'follow' => array('1'=>array('#page_header_enable'))
                 ));
+                ?>  <div id="page_header_enable"><?php
+                    cms_options(array(
+                        'id' => 'header_layout',
+                        'label' => __('Layout',THEMENAME),
+                        'type' => 'imegesselect',
+                        'options' => array(
+                            '' => get_template_directory_uri().'/inc/options/images/header/h-default.png',
+                            '1' => get_template_directory_uri().'/inc/options/images/header/h-default.png',
+                            '2' => get_template_directory_uri().'/inc/options/images/header/h-default.png',
+                        )
+                    ));
+                    $menus = array();
+                    $menus[''] = 'Default';
+                    $obj_menus = wp_get_nav_menus();
+                    foreach ($obj_menus as $obj_menu){
+                        $menus[$obj_menu->term_id] = $obj_menu->name;
+                    }
+                    $navs = get_registered_nav_menus();
+                    foreach ($navs as $key => $mav){
+                        cms_options(array(
+                        'id' => $key,
+                        'label' => $mav,
+                        'type' => 'select',
+                        'options' => $menus
+                        ));
+                    }
+                    ?>
+                    </div>
+                </div>
+                <div id="tabs-page-title">
+                <?php
+                /* page title. */
                 cms_options(array(
-                    'id' => 'page_title_sub_text',
-                    'label' => __('Sub Title',THEMENAME),
-                    'type' => 'text',
+                    'id' => 'page_title',
+                    'label' => __('Page Title',THEMENAME),
+                    'type' => 'switch',
+                    'options' => array('on'=>'1','off'=>''),
+                    'follow' => array('1'=>array('#page_title_enable'))
                 ));
-                cms_options(array(
-                    'id' => 'page_title_type',
-                    'label' => __('Layout',THEMENAME),
-                    'type' => 'imegesselect',
-                    'options' => array(
-                        '' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-0.png',
-                        '1' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-1.png',
-                        '2' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-2.png',
-                        '3' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-3.png',
-                        '4' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-4.png',
-                        '5' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-5.png',
-                        '6' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-6.png',
-                    )
-                ));
-                ?>
+                ?>  <div id="page_title_enable"><?php
+                    cms_options(array(
+                        'id' => 'page_title_text',
+                        'label' => __('Title',THEMENAME),
+                        'type' => 'text',
+                    ));
+                    cms_options(array(
+                        'id' => 'page_title_sub_text',
+                        'label' => __('Sub Title',THEMENAME),
+                        'type' => 'text',
+                    ));
+                    cms_options(array(
+                        'id' => 'page_title_type',
+                        'label' => __('Layout',THEMENAME),
+                        'type' => 'imegesselect',
+                        'options' => array(
+                            '' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-0.png',
+                            '1' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-1.png',
+                            '2' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-2.png',
+                            '3' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-3.png',
+                            '4' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-4.png',
+                            '5' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-5.png',
+                            '6' => get_template_directory_uri().'/inc/options/images/pagetitle/pt-s-6.png',
+                        )
+                    ));
+                    ?>
+                    </div>
                 </div>
             </div>
         </div>
