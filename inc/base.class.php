8c58c4c6acef46bde597460226acec9605ba6e89
Fox <thienduongvangem.ntt@gmail.com>
2015-04-23 16:45:15 +0700

    ok

--- inc/base.class.php
+++ inc/base.class.php
@@ -248,7 +248,7 @@ class CMS_Base
             $font_weight =  !empty($googlefont['font-weight']) ? "font-weight:'".esc_attr($googlefont['font-weight'])."';" : '';
                 
             if(!empty($selecter)){
-                echo esc_attr($selecter)."{font-family:'".esc_attr($googlefont['font-family'])."';'".$font_weight."'}";
+                echo esc_attr($selecter)."{font-family:'".esc_attr($googlefont['font-family'])."';".$font_weight."}";
             }
         }
     }
