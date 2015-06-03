057d4e643f7d848f5a1f67cb3c441628ad33c98b
Fox <thienduongvangem.ntt@gmail.com>
2015-06-03 11:21:33 +0700

    ok

--- functions.php
+++ functions.php
@@ -456,6 +456,34 @@ function cms_save_meta_boxes($post_id) {
 }
 
 add_action('save_post', 'cms_save_meta_boxes');
+
+/**
+ * Display navigation to next/previous comments when applicable.
+ *
+ * @since 1.0.0
+ */
+function cms_comment_nav() {
+    // Are there comments to navigate through?
+    if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+    ?>
+	<nav class="navigation comment-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php _e( 'Comment navigation', THEMENAME ); ?></h2>
+		<div class="nav-links">
+			<?php
+				if ( $prev_link = get_previous_comments_link( __( 'Older Comments', THEMENAME ) ) ) :
+					printf( '<div class="nav-previous">%s</div>', $prev_link );
+				endif;
+
+				if ( $next_link = get_next_comments_link( __( 'Newer Comments', THEMENAME ) ) ) :
+					printf( '<div class="nav-next">%s</div>', $next_link );
+				endif;
+			?>
+		</div><!-- .nav-links -->
+	</nav><!-- .comment-navigation -->
+	<?php
+	endif;
+}
+
 /**
  * Display navigation to next/previous set of posts when applicable.
  *
