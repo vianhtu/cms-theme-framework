f5c4b8ecacf8f190f5b08fd99438c0e6b18957e9
Fox <vianhtu@joomexp.com>
2015-09-07 11:28:35 +0700

    icon

--- header.php
+++ header.php
@@ -17,7 +17,9 @@
 <meta name="viewport" content="initial-scale=1, width=device-width" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ): ?>
 <link rel="icon" type="image/png" href="<?php echo esc_url($smof_data['favicon_icon']['url']); ?>"/>
+<?php endif; ?>
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
