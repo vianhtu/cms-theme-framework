93c4150cbaabd04a4e537c2670afb503dc89b34f
Fox <vianhtu@joomexp.com>
2015-09-07 11:17:37 +0700

    esc_url

--- inc/header/header.php
+++ inc/header/header.php
@@ -20,7 +20,7 @@
     <div class="container">
         <div class="row">
             <div id="cshero-header-logo" class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                <a href="<?php echo home_url(); ?>"><img alt="" src="<?php echo esc_url($smof_data['main_logo']['url']); ?>"></a>
+                <a href="<?php echo esc_url(home_url()); ?>"><img alt="" src="<?php echo esc_url($smof_data['main_logo']['url']); ?>"></a>
             </div>
             <div id="cshero-header-navigation" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
                 <nav id="site-navigation" class="main-navigation" role="navigation">
