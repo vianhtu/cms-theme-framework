411b8d8c030673f1dc3858690e5274247a7eb21d
Fox <vianhtu@joomexp.com>
2015-10-13 14:23:21 +0700

    ok

--- inc/libs/class-tgm-plugin-activation.php
+++ inc/libs/class-tgm-plugin-activation.php
@@ -521,7 +521,7 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 			$actions['update'] = sprintf(
 				'<a href="%1$s" title="%2$s" class="edit">%3$s</a>',
 				esc_url( $this->get_tgmpa_status_url( 'update' ) ),
-				esc_attresc_html__( 'This plugin needs to be updated to be compatible with your theme.', 'cms-theme-framework' ),
+				esc_html__( 'This plugin needs to be updated to be compatible with your theme.', 'cms-theme-framework' ),
 				esc_html__( 'Update Required', 'cms-theme-framework' )
 			);
 
