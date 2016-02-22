cf2a425c1f48fff5499af62587d04f3615d3097b
Fox <thienduongvangem.ntt@gmail.com>
2016-02-22 11:21:35 +0700

    ok

--- single.php
+++ single.php
@@ -6,28 +6,29 @@
  * @subpackage CMS Theme
  * @since 1.0.0
  */
-
 get_header(); ?>
-<div class="<?php theme_framework_main_class(); ?>">
-    <div class="row">
-        <div id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-            <div id="content" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div class="row">
+            <div id="primary" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                <div id="content" role="main">
 
-                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-                    <?php get_template_part( 'single-templates/single/content', get_post_format() ); ?>
+                        <?php get_template_part( 'single-templates/single/content', get_post_format() ); ?>
 
-                    <?php theme_framework_post_nav(); ?>
+                        <?php theme_framework_post_nav(); ?>
 
-                    <?php comments_template( '', true ); ?>
+                        <?php comments_template( '', true ); ?>
 
-                <?php endwhile; // end of the loop. ?>
+                    <?php endwhile; // end of the loop. ?>
 
-            </div><!-- #content -->
-        </div><!-- #primary -->
-        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-            <?php get_sidebar(); ?>
+                </div><!-- #content -->
+            </div><!-- #primary -->
+            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                <?php get_sidebar(); ?>
+            </div>
         </div>
-    </div>
+    </main>
 </div>
 <?php get_footer(); ?>
