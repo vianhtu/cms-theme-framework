bc848b865318a427835fd5eec76a40342187dd10
Fox <thienduongvangem.ntt@gmail.com>
2016-03-03 08:55:39 +0700

    pl

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,42 +34,6 @@ jQuery(document).ready(function($) {
 	 */
 	$(window).on('scroll', function() {
 	});
-
-	/**
-	 * Stiky menu
-	 * 
-	 * Show or hide sticky menu.
-	 * @author Fox
-	 * @since 1.0.0
-	 */
-	function cms_stiky_menu() {
-		if (header_top < scroll_top) {
-			switch (true) {
-				case (window_width > 992):
-					header.addClass('header-fixed');
-					$('body').addClass('fixed-margin-top');
-					break;
-				case ((window_width <= 992 && window_width >= 768) && (CMSOptions.menu_sticky_tablets == '1')):
-					header.addClass('header-fixed');
-					$('body').addClass('fixed-margin-top');
-					break;
-				case ((window_width <= 768) && (CMSOptions.menu_sticky_mobile == '1')):
-					header.addClass('header-fixed');
-					$('body').addClass('fixed-margin-top');
-					break;
-			}
-		} else {
-			header.removeClass('header-fixed');
-			$('body').removeClass('fixed-margin-top');
-		}
-	}
-	function cms_stiky_menu_fixed_page() {
-		if (header_top < scroll_top) {
-			header.addClass('header-fixed-page-trans');
-		} else {
-			header.removeClass('header-fixed-page-trans');
-		}
-	}
 	
 	/**
 	 * Mobile menu
@@ -87,35 +51,4 @@ jQuery(document).ready(function($) {
 			navigation.removeClass('collapse');
 		}
 	});
-
-	/* check mobile screen. */
-	function cms_mobile_menu() {
-		var menu = $('#cshero-header-navigation');
-		
-		/* active mobile menu. */
-		switch (true) {
-		case (window_width <= 992 && window_width >= 768):
-			menu.removeClass('phones-nav').addClass('tablets-nav');
-			/* */
-			cms_mobile_menu_group(menu);
-			break;
-		case (window_width <= 768):
-			menu.removeClass('tablets-nav').addClass('phones-nav');
-			break;
-		default:
-			menu.removeClass('mobile-nav tablets-nav');
-			menu.find('li').removeClass('mobile-group');
-			break;
-		}	
-	}
-	/* group sub menu. */
-	function cms_mobile_menu_group(nav) {
-		nav.each(function(){
-			$(this).find('li').each(function(){
-				if($(this).find('ul:first').length > 0){
-					$(this).addClass('mobile-group');
-				}
-			});
-		});
-	}
 });
