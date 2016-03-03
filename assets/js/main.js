451cb4ea2ba4816b6d00962537c07157a43e71c3
Fox <thienduongvangem.ntt@gmail.com>
2016-03-03 09:06:49 +0700

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function($) {
 	"use strict";
+
+	/* window */
+	var window_width, window_height, scroll_top;
+
+	/* admin bar */
+	var adminbar = $('#wpadminbar');
+	var adminbar_height = 0;
+
+	/* header menu */
+	var header = $('#cshero-header');
+	var header_top = 0;
+
+	/* scroll status */
+	var scroll_status = '';
+
 	/**
 	 * window load event.
 	 * 
@@ -7,6 +22,23 @@ jQuery(document).ready(function($) {
 	 * @author Fox
 	 */
 	$(window).on('load', function() {
+		/** current scroll */
+		scroll_top = $(window).scrollTop();
+
+		/** current window width */
+		window_width = $(window).width();
+
+		/** current window height */
+		window_height = $(window).height();
+
+		/* get admin bar height */
+		adminbar_height = adminbar.length > 0 ? adminbar.outerHeight(true) : 0 ;
+
+		/* get top header menu */
+		header_top = header.length > 0 ? header.offset().top - adminbar_height : 0 ;
+
+		/* check sticky menu. */
+		cms_stiky_menu();
 	});
 
 	/**
@@ -24,6 +56,17 @@ jQuery(document).ready(function($) {
 	 * @author Fox
 	 */
 	$(window).on('resize', function(event, ui) {
+		/** current window width */
+		window_width = $(event.target).width();
+
+		/** current window height */
+		window_height = $(window).height();
+
+		/** current scroll */
+		scroll_top = $(window).scrollTop();
+
+		/* check sticky menu. */
+		cms_stiky_menu();
 	});
 	
 	/**
@@ -33,8 +76,30 @@ jQuery(document).ready(function($) {
 	 * @author Fox
 	 */
 	$(window).on('scroll', function() {
+		/** current scroll */
+		scroll_top = $(window).scrollTop();
+
+		/* check sticky menu. */
+		cms_stiky_menu();
 	});
-	
+
+	/**
+	 * Stiky menu
+	 *
+	 * Show or hide sticky menu.
+	 * @author Fox
+	 * @since 1.0.0
+	 */
+	function cms_stiky_menu() {
+		if (header_top < scroll_top && window_width > 991) {
+			header.addClass('header-fixed');
+			$('body').addClass('hd-fixed');
+		} else {
+			header.removeClass('header-fixed');
+			$('body').removeClass('hd-fixed');
+		}
+	}
+
 	/**
 	 * Mobile menu
 	 * 
